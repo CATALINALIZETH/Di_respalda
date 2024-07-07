6c14c5cc14618e3b09b2ee92073b62ad4e660f83
CATALINALIZETH <114926059+CATALINALIZETH@users.noreply.github.com>
2024-07-06 23:01:43 -0600

    segundo respaldo

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import { useState } from "react";
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
@@ -11,41 +12,125 @@ function App() {
   const [correo, setCorreo] = useState("");
   const [contrasena, setContrasena] = useState("");
 
-  function add() {
+  const [correoLogin, setCorreoLogin] = useState("");
+  const [contrasenaLogin, setContrasenaLogin] = useState("");
+  const [loginStatus, setLoginStatus] = useState(""); //para saber si esta dentro o tiene incorrecto datos
+
+  //lista usuarios
+  const [usuariosList, setUsuarios] = useState([]);
+
+  const add = () => {
 
     axios.post("http://localhost:3001/create", {
       nombre: nombre, //la variable que la representa es nombre esta en el const
       correo: correo,
       contrasena: contrasena
     }).then(() => {
+      //getusuarios();
       alert("El usuario esta registrado");
-    }) //despues de enviar los datos hara la alerta
-      .catch(error => {
-        console.error('Hubo un error!', error);
-      });
+    }).catch(error => {
+      // Se ejecuta cuando hay un error en la solicitud
+      console.error('Hubo un error!', error);
+      alert('Hubo un error al registrar el usuario.');
+    });
+  }
+
+  const getusuarios = () => {
+    axios.get("http://localhost:3001/usuarios").then((response) => {
+      setUsuarios(response.data);
+    });
+  }
+
+  //getusuarios();
+
+  const login = () => {
+    axios.post("http://localhost:3001/login", {
+      correo: correoLogin,
+      contrasena: contrasenaLogin
+    }).then((response) => {
+      if (response.data.auth) {
+        setLoginStatus("Estas Dentro :)");
+      } else {
+        setLoginStatus(response.data.message);
+      }
+    });
   }
 
   return (
-    <div className="App">
-      <div className="datos">
-        <label>Nombre: <input 
-        onChange={(event)=>{
-          setNombre(event.target.value); //extrae la infor del componente
-        }}
-        type="text"></input></label>
-        <label>Correo: <input 
-        onChange={(event)=>{
-          setCorreo(event.target.value); //extrae la infor del componente
-        }}
-        type="text"></input></label>
-        <label>Contraseña: <input
-        onChange={(event)=>{
-          setContrasena(event.target.value); //extrae la infor del componente
-        }}
-         type="password"></input></label>
-        <button onClick={add}>Registrar</button>
+    <div class="container">
+      <br></br>
+      <div className="App">
+        <div className='lista'>
+          {//map recorre toda la lista
+            usuariosList.map((val, key) => {
+              return <div className=''>{val.nombre}</div>
+            })
+          }
+        </div>
       </div>
-    </div>
+      <div class="card text-center">
+        <div class="card-header">
+          Usuarios de Libreria
+        </div>
+        <div className="card-body">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Nombre:</span>
+            <input type="text"
+              onChange={(event) => {
+                setNombre(event.target.value); //extrae la infor del componente
+              }}
+              className="form-control" placeholder="Escribe nombre" aria-label="Username" aria-describedby="basic-addon1"/>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Correo:</span>
+            <input type="text"
+              onChange={(event) => {
+                setCorreo(event.target.value); //extrae la infor del componente
+              }}
+              className="form-control" placeholder="Escribe correo" aria-label="Username" aria-describedby="basic-addon1"/>
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Contraseña:</span>
+            <input type="password"
+            onChange={(event) => {
+              setContrasena(event.target.value); //extrae la infor del componente
+            }}
+              className="form-control" placeholder="Escribe contraseña" aria-label="Username" aria-describedby="basic-addon1"/>
+          </div>
+        </div>
+        <div class="card-footer text-body-secondary">
+          <button className='btn btn-success' onClick={add}>Registrar</button>
+        </div>
+      </div>
+      <br></br>
+{/* sesion inicia */}
+      <div className="card text-center">
+        <div className="card-header">
+          Iniciar Sesión
+        </div>
+        <div className="card-body">
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Correo:</span>
+            <input type="text"
+              onChange={(event) => {
+                setCorreoLogin(event.target.value);
+              }}
+              className="form-control" placeholder="Escribe correo" aria-label="Correo" aria-describedby="basic-addon1" />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text" id="basic-addon1">Contraseña:</span>
+            <input type="password"
+              onChange={(event) => {
+                setContrasenaLogin(event.target.value);
+              }}
+              className="form-control" placeholder="Escribe contraseña" aria-label="Contraseña" aria-describedby="basic-addon1" />
+          </div>
+          <button className="btn btn-primary" onClick={login}>Iniciar Sesión</button>
+          <h3>{loginStatus}</h3>
+        </div>
+      </div>
+
+    </div> //dic container
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app=express();
 const mysql=require("mysql");
 const cors=require("cors");
+const bcrypt = require("bcryptjs"); // Agregar bcrypt
+const jwt = require("jsonwebtoken"); 
 
 app.use(cors());
 app.use(express.json());//toda la info va a pasar a ser un json
@@ -16,10 +18,10 @@ const db = mysql.createConnection({
 app.post("/create",async (req,res)=>{
     const nombre = req.body.nombre; //post/auth/register
     const correo = req.body.correo; //post/auth/register
-    const contrasena = req.body.contrasena; //post/auth/register
-    //const contrasena = await bcrypt.hash(contrasena, 10);
+    let contrasena = req.body.contrasena; //post/auth/register
+    contrasena = await bcrypt.hash(contrasena, 10);
 
-    db.query('INSERT INTO usuarios_libreria(nombre,correo,password) VALUES(?,?,?)',[nombre, correo, contrasena],
+    db.query('INSERT INTO usuarios(nombre,correo,password) VALUES(?,?,?)',[nombre, correo, contrasena],
         (err, result)=>{ //capta rl error o resultado
             if(err){
                 console.log(err);//para que en consola muestre el error
@@ -31,22 +33,41 @@ app.post("/create",async (req,res)=>{
     //para guardar la información del formulario
 });
 
-app.post("/create",async (req,res)=>{
-    const nombre = req.body.nombre; //post/auth/register
-    const correo = req.body.correo; //post/auth/register
-    const contrasena = req.body.contrasena; //post/auth/register
-    //const contrasena = await bcrypt.hash(contrasena, 10);
-
-    db.query('INSERT INTO usuarios_libreria(nombre,correo,password) VALUES(?,?,?)',[nombre, correo, contrasena],
+app.get("/usuarios",async (req,res)=>{
+    db.query('SELECT * FROM usuarios' ,
         (err, result)=>{ //capta rl error o resultado
             if(err){
                 console.log(err);//para que en consola muestre el error
             }else{
-                res.send("Usuario registrado con éxito :)");
+                res.send(result);
             }
         }
     );
     //para guardar la información del formulario
+});
+
+app.post("/login", async (req, res) => {
+    const correo = req.body.correo;
+    const contrasena = req.body.contrasena;
+
+    db.query('SELECT * FROM usuarios WHERE correo = ?', [correo], async (err, result) => {
+        if (err) {
+            res.send({ err: err });
+        }
+
+        if (result.length > 0) {
+            const isMatch = await bcrypt.compare(contrasena, result[0].password);
+            if (isMatch) {
+                // Generar un token de autenticación
+                const token = jwt.sign({ id: result[0].id }, 'secret_key', { expiresIn: '1h' });
+                res.json({ auth: true, token: token });
+            } else {
+                res.json({ auth: false, message: "Usuario o Contraseña incorrectos" });
+            }
+        } else {
+            res.json({ auth: false, message: "No existe el usuario" });
+        }
+    });
 });
 
 app.listen(3001,()=>{
